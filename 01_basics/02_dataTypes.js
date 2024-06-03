75a915302395d76493e9b72bc3c101939cc745ae
kuldeepc18 <choudharykuldeep1803@gmail.com>
2024-06-03 16:19:00 +0530

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,1 +1,29 @@
-console.log("Hello World!")
+"use strict"; // treat all JS code as newer version , not compulsory to write 
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Kuldeep")
+
+
+let name = "Kuldeep"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // prints undefined
+console.log(typeof null); // prints object
