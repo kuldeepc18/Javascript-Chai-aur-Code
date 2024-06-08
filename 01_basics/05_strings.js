509193f0c5c89ea3d5c28e9a9f23ae3347b27515
kuldeepc18 <choudharykuldeep1803@gmail.com>
2024-06-08 13:58:51 +0530

    string explaination

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,10 +1,12 @@
 const name = "kuldeep"
 const repoCount = 50
 
-// console.log(name + repoCount + " Value");
+// console.log(name + repoCount + " Value");    //outdated syntax
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);      //use this syntax for string concatenation 
 
+
+//another method to declare string
 const gameName = new String('kuldeep-kc-com')
 
 // console.log(gameName[0]);
@@ -16,20 +18,20 @@ const gameName = new String('kuldeep-kc-com')
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('t'));
 
-const newString = gameName.substring(0, 4)
-console.log(newString);
+const newString = gameName.substring(0, 4)        //prints string from 0 - 3 
+console.log(newString);                         //substring ignores negative values 
 
 const anotherString = gameName.slice(-8, 4)
 console.log(anotherString);
 
 const newStringOne = "   kuldeep    "
 console.log(newStringOne);
-console.log(newStringOne.trim());
+console.log(newStringOne.trim());           //trim removes starting and ending space
 
 const url = "https://kuldeep.com/kuldeep%20choudhary"
 
 console.log(url.replace('%20', '-'))
 
-console.log(url.includes('sundar'))
+console.log(url.includes('sun'))         //returns true or false
 
-console.log(gameName.split('-'));
+console.log(gameName.split('-'));        //returns an array after spliting at '-'
