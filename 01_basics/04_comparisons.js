5a91289e43a28a7817d717ae7cc04feaf813de0d
kuldeepc18 <choudharykuldeep1803@gmail.com>
2024-06-26 10:20:31 +0530

    added difference between == and ===

--- 01_basics/04_comparisons.js
+++ 01_basics/04_comparisons.js
@@ -19,3 +19,8 @@ console.log(undefined < 0);
 // === --> it compares numbers/variables along with its datatype 
 
 console.log("2" === 2);
+
+// In JavaScript, the == operator is an abstract equality comparison operator that checks 
+// if two variables have the same value. 
+// The === operator is a strict equality comparison operator 
+// that checks if two variables have the same value and data type.
