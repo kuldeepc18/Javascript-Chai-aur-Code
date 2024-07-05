c09bf2479b54d74946d812a8ff45648efe630462
kuldeepc18 <choudharykuldeep1803@gmail.com>
2024-07-05 14:07:54 +0530

    project 2 solution code

--- 07_projects/02_BMICalculator/script.js
+++ 07_projects/02_BMICalculator/script.js
@@ -1,49 +1,3 @@
-# Project related to DOM
-
-## project link
-[Click here](https://stackblitz.com/edit/dom-project-chaiaurcode?file=index.html)
-
-# Solution Code
-
-## project 1
-
-```javascript
-
-const buttons = document.querySelectorAll(".button")
-const body = document.querySelector("body")
-
-buttons.forEach(function (button) {
-    console.log(button);
-    button.addEventListener("click", function (e) {
-        console.log(e);
-        console.log(e.target);
-        if (e.target.id === 'grey') {
-            body.style.backgroundColor = e.target.id;
-        }
-        if (e.target.id === 'white') {
-            body.style.backgroundColor = e.target.id;
-        }
-        if (e.target.id === 'blue') {
-            body.style.backgroundColor = e.target.id;
-        }
-        if (e.target.id === 'yellow') {
-            body.style.backgroundColor = e.target.id;
-        }
-        if (e.target.id === 'red') {
-            body.style.backgroundColor = e.target.id;
-        }
-        if (e.target.id === 'brown') {
-            body.style.backgroundColor = e.target.id;
-        }
-
-    })
-})
-
-```
-## project 2
-
-```javascript
-
 const form = document.querySelector("form")
 
 // this will give you empty value
@@ -79,5 +33,3 @@ form.addEventListener("submit", function (e) {
     }
 
 })
-
-```
