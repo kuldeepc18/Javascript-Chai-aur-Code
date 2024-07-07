14bfa5fbd54cad037e030ea972953bfd068fea0d
kuldeepc18 <choudharykuldeep1803@gmail.com>
2024-07-08 00:46:20 +0530

    file location updated

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -3,15 +3,17 @@ let score = "kuldeep"
 //console.log(typeof score);
 //console.log(typeof(score));
 
+//String to Number
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // "33" => 33
 // "33abc" => NaN
 //  true => 1; false => 0
 
+//String to Boolean 
 let isLoggedIn = "kuldeep"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
@@ -33,12 +35,12 @@ let value = 3
 let negValue = -value
 // console.log(negValue);
 
-// console.log(2+2);
-// console.log(2-2);
-// console.log(2*2);
-// console.log(2**3);
-// console.log(2/3);
-// console.log(2%3);
+// console.log(2+2);       //addition 
+// console.log(2-2);       //subtraction
+// console.log(2*2);       //multiplication
+// console.log(2**3);      //exponential
+// console.log(2/3);       //division
+// console.log(2%3);       //modulus 2(mod 3) = 2
 
 let str1 = "hello"
 let str2 = " kuldeep"
@@ -59,10 +61,11 @@ let str3 = str1 + str2
 let num1, num2, num3
 
 num1 = num2 = num3 = 2 + 2
+// console.log(`num1 is ${num1}\nnum2 is ${num2}\nnum3 is ${num3} `);
 
 let gameCounter = 100
 ++gameCounter;
-// console.log(gameCounter);
+console.log(gameCounter);
 
 // link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
