c2ab0e8abc651be4ee5b74b726c227c0f933d18c
kuldeepc18 <choudharykuldeep1803@gmail.com>
2024-07-23 14:16:30 +0530

    object based getter and setter method

--- 10_classes_and_oop/12_object_get_set.js
+++ 10_classes_and_oop/12_object_get_set.js
@@ -1,5 +1,7 @@
 // very rarely used
 
+
+// private properties are properties of a class that are only accessible from within the class itself. They cannot be accessed or modified from outside the class, ensuring encapsulation and data privacy.
 const User = {
     _email: "k@kc.com",   //underscore is just like a private property
     _password: "abc",
